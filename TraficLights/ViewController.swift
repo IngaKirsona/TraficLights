4618880126e18b508116ec2a39b1ce70f5ec393c
IngaKirsona <ingas@inbox.lv>
2020-08-20 16:51:58 +0300

    entered all the buttons

--- TraficLights/ViewController.swift
+++ TraficLights/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startButton: UIButton!
+    var isTapped = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        startButton.layer.cornerRadius = 70
     }
 
-
+    @IBAction func StartTapped(_ sender: Any) {
+        if isTapped {
+            self.isTapped = true
+            self.startButton.setTitle("NEXT", for: .normal)
+        }
+    }
+    
 }
 

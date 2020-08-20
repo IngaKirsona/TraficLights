70dfde8fe8ce2512c3b04579c75031649b176e4d
IngaKirsona <ingas@inbox.lv>
2020-08-20 12:08:10 +0300

    Initial Commit

---
+++ TraficLights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TraficLights
+//
+//  Created by Inga Kirsona on 20/08/2020.
+//  Copyright © 2020 Inga Kirsona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
